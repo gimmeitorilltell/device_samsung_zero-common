69aba849e65c255e3855b59d907811426f22ef90
Kevin F. Haggerty <haggertk@lineageos.org>
2017-12-25 15:51:39 -0700

    zero-common: Update RIL_REQUEST_QUERY_AVAILABLE_NETWORKS response prop
    * mQANElements never made tremendous sense, the response strings
      are not part of an object, just a text string making up a parcel.
    * ro.ril.telephony.mqanelements -> ro.ril.telephony.qan_resp_strings
      for consistency with some of the other new RIL props.
    
    Change-Id: I6f941f5177852454a4d4b9494077329814d884f1

---
+++ configs/product/radio.mk
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Radio
+PRODUCT_PACKAGES += \
+	libxml2 \
+	libprotobuf-cpp-full \
+	rild \
+	libreference-ril \
+	libril \
+	libsecril-client \
+	libsecril-client-sap \
+	android.hardware.radio@1.0 \
+	android.hardware.radio.deprecated@1.0 \
+	modemloader
+
+# Radio Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.add_power_save=1 \
+    persist.radio.apm_sim_not_pwdn=1 \
+    rild.libpath=/system/lib64/libsec-ril.so \
+    rild.libpath2=/system/lib64/libsec-ril-dsds.so \
+    telephony.lteOnGsmDevice=1 \
+    telephony.lteOnCdmaDevice=0 \
+    ro.telephony.default_network=9 \
+    ro.use_data_netmgrd=false \
+    persist.data.netmgrd.qos.enable=false \
+    ro.ril.hsxpa=1 \
+    ro.ril.telephony.qan_resp_strings=6 \
+    ro.ril.gprsclass=10 \
+    ro.telephony.get_imsi_from_sim=true \
+    ro.ril.force_eri_from_xml=true \
+    net.tethering.noprovisioning=true
+
+# Dual-SIM Support
+ifeq ($(BOARD_HAS_DUAL_SIM),true)
+    include $(LOCAL_PATH)/configs/radio/dual/product.mk
+else
+    include $(LOCAL_PATH)/configs/radio/single/product.mk
+endif
