29367e3698c228280465575f1295bfe0669a8600
gimme <gimmeitorilltell@gmail.com>
2018-02-16 05:08:28 -0500

    zero: Let the hardare provide the RIL

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -171,7 +171,7 @@ TARGET_SLSI_VARIANT := cm
 TARGET_POWERHAL_VARIANT := samsung
 
 # Radio
-BOARD_PROVIDES_LIBRIL := true
+#BOARD_PROVIDES_LIBRIL := true
 BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
 
 # Recovery
